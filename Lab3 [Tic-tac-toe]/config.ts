be0c20d20d26ea5e2e60258f0d5570258cd67e0c
gqggqg <71404862+gqggqg@users.noreply.github.com>
2021-06-29 22:50:52 +0200

    separation into class files

---
+++ Lab3 [Tic-tac-toe]/config.ts
@@ -0,0 +1,8 @@
+const NO_HOVER_CLASS: string = "no-hover";
+const HIDDEN_CLASS: string = "hidden";
+
+const HOVER_CONTENT_ATTR = "hover-content";
+
+const DRAW_MESSAGE: string = "Ничья! ;)";
+
+const PARSE_INT_BASE: number = 10;
